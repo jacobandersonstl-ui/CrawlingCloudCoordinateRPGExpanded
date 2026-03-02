2c44546cd8bb228051149acfd455de85b205a4e0
fives9335 <jacobandersonstl@gmail.com>
2026-03-01 20:42:42 -0600

    Updated Main Source

--- #CrawlingCloudCoordinateRPG_Ev2.py
+++ #CrawlingCloudCoordinateRPG_Ev2.py
@@ -1,9 +1,10 @@
+print("PRE-ALPHA BUILT VERSION 0.0.3 NOTHING IS CONFIRMED TO STAY. EXPECT CHANGES IN THE FULL RELEASE")
 #CrawlingCloudCoordinateRPG_Ev2
 input("\nWelcome to CrawlingCloudCoordinateRPG_Ev2! This is the full story version of the original CrawlingCloudStoryEngine. Please play the previous game by me, CrawlingCloudCoordinateRPG, even though it's not very good. I'd personally appreciate if you went back and checked out my other projects! Have Fun!")
 
 
-x = -31
-y = 35
+x = 0
+y = 0
 main_story_chapter = 0
 name = None
 gender = None
@@ -11,26 +12,52 @@ job = None
 stamina = 5
 health = 100
 potions = 0
+move = None
+password = None
+chapter_4_complete = False
+
+def passowrd_opening():
+    global password
+    password = input("\nWAIT! If you're starting from a password save, then please type it in now. If not, then just hit  Enter to start a new game! Password: ")
+    if password == "lcabbahabkst":
+        input("\nPassword Accetped. You will start at character creation because this script doesn't remember what you entered last time. Press Enter.")
+        opening()
+    elif password == "abahbsctlkab":
+        input("\nPassword Accetped. You will start at character creation because this script doesn't remember what you entered last time. Press Enter.")
+        opening()
+    elif password == "bbckshatalba":
+        input("\nPassword Accetped. You will start at character creation because this script doesn't remember what you entered last time. Press Enter.")
+        opening()
+    else:
+        opening()
 
 def opening():
     print("opening working")
     global x, y, main_story_chapter, name, gender, job, stamina
-    input("\nYou lay asleep in your bed......... AHHH! Well, you're awake now.")
-    input("\nYOo stand and go to your mirror.")
+    input("\nYou wake up in your tent in the middle of the woods.")
+    input("\nYou stand and go to your mirror.")
     name = input("\nWhat is your name? Type here: --> ")
     gender = input("\nAre you male or female? Type Here: --> ").strip().upper()
     job = input("\nAnd your job/proffession? You can choose: Engineer, chef, military man/woman, or blacksmith. Type Here: --> ").strip().upper()
+    print("\n")
     print("Okay, you are", name, "and you are a ", gender, "who does ", job, "for work?")
     char_create_y_n = input("\nIs this information correct? Yes or No: --> ").lower().strip()
     if char_create_y_n == "yes":
         input("\nOkay! Lets Move on!")
-        chapter_1()
+        if char_create_y_n == "yes" and password == "abahbsctlkab":
+            input("\nWell, you're back off onto your adventure. Good Luck Kid.")
+            chapter_2()
+        elif char_create_y_n == "yes" and password == "bbckshatalba":
+            input("\nWell, you're back off onto your adventure. Good Luck Kid.")
+            chapter_3()
+        else:
+            chapter_1()
     elif char_create_y_n == "no":
         input("\nLets restart then."), opening()
 
 def chapter_1():
     print("c1 working")
-    global x, y, main_story_chapter, stamina, health, potions
+    global x, y, main_story_chapter, stamina, health, potions, move
     main_story_chapter = 1
 
     chapter_playing = True
@@ -44,7 +71,7 @@ def chapter_1():
 
     while chapter_playing:
         print("Status Bar: Health:", health, "Potions:", potions, "X:", x, "Y:", y, "Name:", name, "Stamina:", stamina, ". You can go North, East, South, or West with the W, A, S, and D keys. You can use potions with the P key.")
-        move = input("So,  where too? W (North), A (West), S (South), or D (East)?").strip()
+        move = input("So,  where too? W (North), A (West), S (South), or D (East)? ! space = 5 meters").strip()
         
         if move == "N":
             print("Ok! Going North!")
@@ -91,8 +118,15 @@ def chapter_1():
                 stamina += 1
             #moves += 1
         elif move == "Q":
-            print("Ok! Bye!")
-            quit()
+            quit_y_n = input("\nAre you sure you want to quit? Y/N: ").upper().strip()
+            if quit_y_n == "Y":
+                input("\nWAIT! Wtite this password down so you can get back to this level when you return: lcabbahabkst")
+                print("\nOk, Bye!")
+                chapter_playing = False
+                quit()
+            elif quit_y_n == "N":
+                input("\nThen Let's get you back in there!")
+
         if stamina < 0:
             stamina = 0
 
@@ -128,23 +162,27 @@ def chapter_1():
             input("The fog is way to thick to go South.")
 
         if x == -31 and y == 35:
-            print("\n" + "="*50)
-            print("                You made it to the Capital, hooray!")
-            print("="*50)
+            print("\n" + "="*140)
+            print("                As you walk into the barren streets of Yirelem, you see the giant marble buildings. Let's get to the castle.")
+            print("="*140)
             print("\n"*2)
 
-            input("\n")
-            
-            print("\n" + "="*50)
+            input("\nPress Enter to Proceed.")
+
+            print("\n" + "="*125)
             print("                Act I End")
-            print("="*50)
+            print("="*125)
             print("\n"*2)
+
+            input("\nPress Enter to begin Chapter II")
+
             chapter_playing = False
             chapter_2()
 
 
 def chapter_2():
     global x, y, main_story_chapter
+    chapter_2_playing = True
     print("c2 working")
     main_story_chapter = 2
     chapter_3()
@@ -152,17 +190,20 @@ def chapter_2():
 def chapter_3():
     print("c3 working")
     global x, y, main_story_chapter
+    chapter_3_playing = True
     main_story_chapter = 3
     ending()
 
 def ending():
-    global x, y, main_story_chapter
+    global x, y, main_story_chapter, chapter_4_complete
     print("ending working")
+    chapter_4_playing = True
     main_story_chapter = 4
+    chapter_4_complete = True
 
 while True:
-
     if main_story_chapter == 0:
-        opening()
-    elif main_story_chapter == 4:
+        passowrd_opening()
+    elif main_story_chapter == 4 and chapter_4_complete == True:
+        input("\nThank you for playing The Crawling CLoud Pre-Alpha Version 0.0.3. This is my first serious game project I've published on GitHub and really want to thank everyone for their support. Stay tuned for future alphas/betas and eventually the FULL RELEASE!!!")
         quit()
